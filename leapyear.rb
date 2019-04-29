a5677a0a5f3ff09dea933896fde2ff9a0ca31bfa
anton feldmann <anton.feldmann@outlook.de>
2019-04-29 11:55:58 +0200

    on line function

--- leapyear.rb
+++ leapyear.rb
@@ -1,10 +1,18 @@
 #!/usr/bin/env ruby 
 
 n=ARGV[0]
+y=n.to_i || 0
 
-if ((n & 127) == 0 or (n & 624999) == 0)
-    puts "not leap year" 
-elsif (n & 3) == 0  
+# in the julian calender has each 4 year a leap year. 
+# to test in a computer if a number is a multiple of 
+# 4 is to check id the last two diges are zero
+
+# If in the julian calender a year is multiplicative of 128,
+# then that year is no leap year.
+# for a computer it is easy to check for multiple of 128.
+# check if the last 7 bits are 0
+
+if (y & 127) != 0 && (y & 3) == 0  
     puts "leap year" 
 else  
     puts "not leap year"
